626231635890d541f0b1e839fcc3940878be5566
ravi barla <ravibarla1@gmail.com>
2023-09-14 22:04:28 +0530

    updating styles

--- src/component/Habit.js
+++ src/component/Habit.js
@@ -7,10 +7,10 @@ const Habit = (props) => {
   const navigate = useNavigate();
   return (
     <div
-      className="d-flex flex-row justify-content-between border  border-primary-subtle h-50 px-2 my-2 mx-2 w-100"
+      className="d-flex flex-row  justify-content-between  border  border-primary-subtle rounded-top rounded-bottom rounded-end border-2 h-50 px-2 my-2 mx-2 w-100"
       onClick={() => navigate(`tracker/${habit.id}`)}
     >
-      <div className="text-md-end">{habit.name}</div>
+      <div className="text-md-end justify-content-center">{habit.name}</div>
       <Rating updateHistory={habit.updateHistory} />
       <RatingFraction updateHistory={habit.updateHistory} />
       <div className="">weekly</div>
